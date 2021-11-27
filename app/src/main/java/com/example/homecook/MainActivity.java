09f80755fae23ad2b773f359088591e8cbddef40
ghazar <ghazar55@gmail.com>
2021-11-27 11:30:54 -0500

    test by ghazar

--- app/src/main/java/com/example/homecook/MainActivity.java
+++ app/src/main/java/com/example/homecook/MainActivity.java
@@ -6,6 +6,9 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    // test ghazar's branch
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
