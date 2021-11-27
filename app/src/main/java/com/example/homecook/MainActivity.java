bb2aac8eaed11120877232dd1d05afe7a338f5c5
Deeksha28 <deeksha2824@gmail.com>
2021-11-27 12:53:51 -0500

    code review deeksha

--- app/src/main/java/com/example/homecook/MainActivity.java
+++ app/src/main/java/com/example/homecook/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    //Hi Deeksha
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
